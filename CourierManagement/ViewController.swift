7b298749b6e85e1b8bc6ca82bbb10c0f0c594023
karankarthic.n <karankarthic.n@zohocorp.com>
2020-09-25 11:32:01 +0530

    Initial Commit

---
+++ CourierManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourierManagement
+//
+//  Created by Karan Karthic on 25/09/20.
+//  Copyright © 2020 Karan Karthic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
